ad5476d8efb1f7add8c8c3abe275c416d21b80c6
Kate Rybiy <kporhun@gmail.com>
2024-08-02 21:05:54 -0700

    redux-saga simple counter

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Provider } from "react-redux";
+import Counter from "./components/Counter";
+import store from "./redux/store";
+
+
+function App() {
+
+
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  )
+}
+
+export default App
---
+++ src/redux/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
+import createSagaMiddleware from "redux-saga"
+import rootSaga from "./counterSagas";
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer
+  },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(sagaMiddleware)
+  })
+
+sagaMiddleware.run(rootSaga)
+
+export type RootState = ReturnType<typeof store.getState>
+export default store
